5d6da29b36ab71268739db850678bba82ae4b358
difuria <69213222+difuria@users.noreply.github.com>
2020-11-15 23:46:40 +0000

    Added auto background update

--- setup.sh
+++ setup.sh
@@ -26,11 +26,18 @@ source ~/.bashrc
 
 # Add something to download image and change background
 
-# Python bits 
-echo "Installing and updating Python packages" 
-pip install --upgrade pip 
- 
-# Reboot 
-echo "Rebooting system to ensure changes have taken effect" 
-sleep(5) 
+# Python bits
+echo "Installing and updating Python packages"
+pip install --upgrade pip
+
+# Update background
+cd ~
+wget https://raw.githubusercontent.com/difuria/Raspberry-Pi-Setup/main/Sorrento%20%5B2019%5D%20-%2010.jpg
+background_image='Sorrento [2019] - 10.jpg'
+mv "$background_image" Pictures/
+pcmanfm --set-wallpaper `pwd`/Pictues/"$background_image"
+
+# Reboot
+echo "Rebooting system to ensure changes have taken effect"
+sleep(5)
 sudo reboot
