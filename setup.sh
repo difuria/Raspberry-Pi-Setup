a09f8c26d8b7499ba79679a4bef2bb9343c48271
difuria <domdifuria@gmail.com>
2024-01-07 19:52:23 +0000

    Add latest setup run

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ sudo apt autoremove -y
 # Linux tools
 echo "Installing Linux Tools"
 sudo apt-get install ack-grep -y
+sudo apt-get install gh -y
 sudo apt-get install locate -y
 sudo apt-get install vim -y
 sudo apt-get install zsh -y
-sudo apt-get install realvnc-vnc-server -y
 
 # If the python version installed is less than 3 set up an alias for it
 python_version=`echo "import sys
@@ -32,16 +32,7 @@ source ~/.bashrc
 # Python bits
 echo "Installing and updating Python packages"
 pip install --upgrade pip
-pip install pymongo
-pip install dnspython
 pip install pipenv
-
-# Update background
-cd ~
-wget https://raw.githubusercontent.com/difuria/Raspberry-Pi-Setup/main/Sorrento%20%5B2019%5D%20-%2010.jpg
-background_image='Sorrento [2019] - 10.jpg'
-mv "$background_image" Pictures/
-pcmanfm --set-wallpaper `pwd`/Pictues/"$background_image"
 
 # HEIC isn't viewable with the current packages
 sudo apt install heif-gdk-pixbuf
